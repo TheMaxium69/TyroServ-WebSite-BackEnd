df9c188da0e1bd5e56923774efbdbab0bc30e721
AnaelTech <142304772+AnaelTech@users.noreply.github.com>
2024-09-05 17:33:58 +0200

    update project

--- public/index.php
+++ public/index.php
@@ -2,7 +2,11 @@
 
 use App\Kernel;
 
-require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
+header('Access-Control-Allow-Origin: http://localhost:4200');
+header('Access-Control-Allow-Methods: GET, POST, OPTIONS, DELETE, PUT');
+header('Access-Control-Allow-Headers: Content-Type, Authorization');
+
+require_once dirname(__DIR__) . '/vendor/autoload_runtime.php';
 
 return function (array $context) {
     return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
