c1e555cb15d04b91bcee2e5370f779b99f1daba3
TheMaximeSan <mxm.tournier@gmail.com>
2024-09-15 16:27:27 +0200

    update

--- src/Controller/RankController.php
+++ src/Controller/RankController.php
@@ -3,16 +3,187 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class RankController extends AbstractController
 {
-    #[Route('/rank', name: 'app_rank')]
-    public function index(): Response
+
+
+    #[Route('/rank-s1-playerPreview/', name: 'app_rank_s1_player_preview', methods:['GET'])]
+    public function getRankS1PlayerPreview(): JsonResponse
     {
-        return $this->render('rank/index.html.twig', [
-            'controller_name' => 'RankController',
-        ]);
+
+        $rank = [
+            [
+              "pseudo" => "TheMaximeSan",
+              "money" => "9500,0",
+            ],
+            [
+              "pseudo" => "TheMaxium69",
+              "money" => "9400,0",
+            ],
+            [
+              "pseudo" => "AnaelTech",
+              "money" => "9300,0",
+            ],
+            [
+              "pseudo" => "Michel",
+              "money" => "9200,0",
+            ],
+            [
+              "pseudo" => "Mario",
+              "money" => "9100,0",
+            ]
+        ];
+
+
+        return $this->json(
+            [
+                'status' => 'true',
+                'why' => 'Succes Request',
+                'data' => $rank,
+            ]
+        );
+
     }
+
+    #[Route('/rank-s1-player/', name: 'app_rank_s1_player', methods:['GET'])]
+    public function getRankS1Player(): JsonResponse
+    {
+
+        $rank = [
+            [
+              "pseudo" => "TheMaximeSan",
+              "money" => "9500,0",
+            ],
+            [
+              "pseudo" => "TheMaxium69",
+              "money" => "9400,0",
+            ],
+            [
+              "pseudo" => "AnaelTech",
+              "money" => "9300,0",
+            ],
+            [
+              "pseudo" => "Michel",
+              "money" => "9200,0",
+            ],
+            [
+              "pseudo" => "Mario",
+              "money" => "9100,0",
+            ],
+            [
+              "pseudo" => "Luigi",
+              "money" => "9000,0",
+            ],
+            [
+              "pseudo" => "Luigi",
+              "money" => "9000,0",
+            ],
+            [
+              "pseudo" => "Luigi",
+              "money" => "9000,0",
+            ],
+            [
+              "pseudo" => "Luigi",
+              "money" => "9000,0",
+            ],
+            [
+              "pseudo" => "Luigi",
+              "money" => "9000,0",
+            ],
+            [
+              "pseudo" => "Luigi",
+              "money" => "9000,0",
+            ],
+            [
+              "pseudo" => "Luigi",
+              "money" => "9000,0",
+            ],
+            [
+              "pseudo" => "Luigi",
+              "money" => "9000,0",
+            ],
+            [
+              "pseudo" => "Luigi",
+              "money" => "9000,0",
+            ],
+            [
+              "pseudo" => "Luigi",
+              "money" => "9000,0",
+            ],
+            [
+              "pseudo" => "Luigi",
+              "money" => "9000,0",
+            ],
+            [
+              "pseudo" => "Luigi",
+              "money" => "9000,0",
+            ],
+            [
+              "pseudo" => "Luigi",
+              "money" => "9000,0",
+            ],
+            [
+              "pseudo" => "Luigi",
+              "money" => "9000,0",
+            ],
+            [
+              "pseudo" => "Luigi",
+              "money" => "9000,0",
+            ],
+            [
+              "pseudo" => "Luigi",
+              "money" => "9000,0",
+            ],
+        ];
+
+
+        return $this->json(
+            [
+                'status' => 'true',
+                'why' => 'Succes Request',
+                'data' => $rank,
+            ]
+        );
+
+    }
+
+    #[Route('/rank-s1-faction/', name: 'app_rank_s1_faction', methods:['GET'])]
+    public function getRankS1Faction(): JsonResponse
+    {
+
+        return $this->json(
+            [
+                'status' => 'true',
+                'why' => 'Succes Request',
+                'data' => "",
+            ]
+        );
+
+    }
+
+    #[Route('/rank-s2-player/', name: 'app_rank_s2_player', methods:['GET'])]
+    public function getRankS2Player(): JsonResponse
+    {
+
+        return $this->json(
+            [
+                'status' => 'true',
+                'why' => 'Succes Request',
+                'data' => "",
+            ]
+        );
+
+    }
+
+
+
+
+
+
+
+
 }
--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -8,11 +8,5 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class UserController extends AbstractController
 {
-    #[Route('/user', name: 'app_user')]
-    public function index(): Response
-    {
-        return $this->render('user/index.html.twig', [
-            'controller_name' => 'UserController',
-        ]);
-    }
+
 }
