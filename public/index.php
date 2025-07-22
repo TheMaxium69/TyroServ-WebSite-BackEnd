547e26228e02f629acf16c1c266cca01ac8fb110
TheMaxium69 <mxm.tournier@gmail.com>
2025-07-22 20:34:19 +0200

    update

--- public/index.php
+++ public/index.php
@@ -2,9 +2,9 @@
 
 use App\Kernel;
 
-header('Access-Control-Allow-Origin: http://localhost:4200');
+/*header('Access-Control-Allow-Origin: http://localhost:4200');
 header('Access-Control-Allow-Methods: GET, POST, OPTIONS, DELETE, PUT');
-header('Access-Control-Allow-Headers: Content-Type, Authorization');
+header('Access-Control-Allow-Headers: Content-Type, Authorization');*/
 
 require_once dirname(__DIR__) . '/vendor/autoload_runtime.php';
 
